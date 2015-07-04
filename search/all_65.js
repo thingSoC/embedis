5c6576a67fa921df54777c3f4a75478a475fd3b8
moxondesign <tom@moxon.com>
2015-07-04 15:35:52 -0700

    add to doxygen docs

--- search/all_65.js
+++ search/all_65.js
@@ -5,5 +5,6 @@ var searchData=
   ['embedis_5fdictionary',['embedis_dictionary',['../structembedis__dictionary.html',1,'']]],
   ['embedis_5fdictionary_5fkey',['embedis_dictionary_key',['../structembedis__dictionary__key.html',1,'']]],
   ['embedis_5fprototol_5fstate',['embedis_prototol_state',['../structembedis__prototol__state.html',1,'']]],
-  ['embedis_5fstate',['embedis_state',['../structembedis__state.html',1,'']]]
+  ['embedis_5fstate',['embedis_state',['../structembedis__state.html',1,'']]],
+  ['embedis_20documentation',['Embedis Documentation',['../index.html',1,'']]]
 ];
---
+++ search/pages_65.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['embedis_20documentation',['Embedis Documentation',['../index.html',1,'']]]
+];
