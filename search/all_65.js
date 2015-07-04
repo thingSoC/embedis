d26bed9781aaca19f0dc1fbb079d3bf392b86677
moxondesign <tom@moxon.com>
2015-07-04 15:04:38 -0700

    add to doxygen docs

--- search/all_65.js
+++ search/all_65.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['embedis_2eh',['embedis.h',['../embedis_8h.html',1,'']]],
   ['embedis_5fcommand',['embedis_command',['../structembedis__command.html',1,'']]],
-  ['embedis_5fdict_5fconfig',['embedis_dict_config',['../structembedis__dict__config.html',1,'']]],
-  ['embedis_5fdict_5fhandler',['embedis_dict_handler',['../structembedis__dict__handler.html',1,'']]]
+  ['embedis_5fdictionary',['embedis_dictionary',['../structembedis__dictionary.html',1,'']]],
+  ['embedis_5fdictionary_5fkey',['embedis_dictionary_key',['../structembedis__dictionary__key.html',1,'']]],
+  ['embedis_5fprototol_5fstate',['embedis_prototol_state',['../structembedis__prototol__state.html',1,'']]],
+  ['embedis_5fstate',['embedis_state',['../structembedis__state.html',1,'']]]
 ];
