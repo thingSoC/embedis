6af1279b861a63765221dab1db4867e53d080eb6
moxondesign <tom@moxon.com>
2015-07-04 20:56:14 -0700

    add to doxygen docs

--- search/groups_65.js
+++ search/groups_65.js
@@ -1,4 +1,10 @@
 var searchData=
 [
+  ['embedis',['embedis',['../group__embedis.html',1,'']]],
+  ['embedis_5fexported_5fconstants',['Embedis_Exported_Constants',['../group__embedis___exported___constants.html',1,'']]],
+  ['embedis_5fexported_5ffunctions',['Embedis_Exported_Functions',['../group__embedis___exported___functions.html',1,'']]],
+  ['embedis_5fexported_5fmacros',['Embedis_Exported_Macros',['../group__embedis___exported___macros.html',1,'']]],
+  ['embedis_5fexported_5ftypes',['Embedis_Exported_Types',['../group__embedis___exported___types.html',1,'']]],
+  ['embedis_5fexported_5fvariables',['Embedis_Exported_Variables',['../group__embedis___exported___variables.html',1,'']]],
   ['embedis_20command_20definitions',['Embedis Command Definitions',['../group__main__commands.html',1,'']]]
 ];
--- search/typedefs_65.js
+++ search/typedefs_65.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['embedis_5fstate',['embedis_state',['../embedis_8h.html#acbcc8ef06f4a19ec3145cbb5456956eb',1,'embedis.h']]]
+  ['embedis_5fstate',['embedis_state',['../group__embedis___exported___variables.html#gacbcc8ef06f4a19ec3145cbb5456956eb',1,'embedis.h']]]
 ];
