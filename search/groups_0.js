de02ebb5f3109c1d407198e8384f1a503f93c480
moxondesign <tom@moxon.com>
2016-02-23 16:32:07 -0800

    update doxygen

--- search/groups_0.js
+++ search/groups_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['embedis_20command_20definitions',['Embedis Command Definitions',['../group__main__commands.html',1,'']]]
+  ['embedis',['Embedis',['../group__main.html',1,'']]]
 ];
